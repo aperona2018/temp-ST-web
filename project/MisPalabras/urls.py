93030aaa7b976b14b215f232e1068194f53222a0
aperona2018 <a.perona.2018@alumnos.urjc.es>
2022-06-19 16:12:54 +0200

    Añado cambios de imagen en palabras

--- project/MisPalabras/urls.py
+++ project/MisPalabras/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.index, name="Indice"),
     path('ayuda', views.get_ayuda, name="Ayuda"),
     path('MiPagina', views.get_usersPag, name="MiPagina"),
+    path('error', views.get_error, name="error"),
+    path('json', views.get_json, name="json"),
     path('<str:pal>', views.get_palabra, name="Palabra")
 ]
--- project/MisPalabras/views.py
+++ project/MisPalabras/views.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 import random
 import urllib.request
 from time import timezone
@@ -17,6 +18,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Palabra, Comentario
 
 
+
 # Create your views here.
 
 def conseguirValoresPalabras():
@@ -30,15 +32,19 @@ def conseguirValoresPalabras():
 
     return lista_palabras, num_palabras, palabraRandom
 
+def findfile(name, path):
+    for dirpath, dirname, filename in os.walk(path):
+        if name in filename:
+            return os.path.join(dirpath, name)
+
 
 def crearListaMasVotadas():
     lista_palabras, num_palabras, palabraRandom = conseguirValoresPalabras()
     lista_votadas = sorted(lista_palabras, key=lambda p: p.votos)[::-1]
     lista_mas_votadas = []
-    lista_test = []
     num_pals = 0
     for palabra in lista_votadas:
-        if (num_pals <= 10) or (num_pals <= len(lista_votadas)):
+        if (num_pals < 10):
             lista_mas_votadas.append(palabra)
             num_pals += 1
     return lista_mas_votadas
@@ -89,7 +95,8 @@ def index(request):
                     user.save()
                     login(request, user)
                 except IntegrityError:
-                    print("TRAZA - Error al crear usuario")  # -- TODO: Mensaje de error
+                    url = '/MisPalabras/error'
+                    redirect(url)
 
         if action == 'Logout':
             logout(request)
@@ -125,17 +132,18 @@ def get_palabra(request, pal):
     if request.method == "POST":
         action = request.POST['action']
 
-        if action == 'añadir':
-            body = request.body.decode('utf-8')
-            partes_body = body.split('&')
-            pal = partes_body[1].split('=')[1]
-            definicion = partes_body[2].split('=')[1]
-            autor = request.user
-            votos = 0
-            if len(definicion == 0):
+        if action == 'añadir palabra':
+            text = parsearWiki(pal)[0]
+            # Lo hacemos así para que no haya definiciones a medias
+            textoSplit = text.split('.')
+            definicion = ""
+            for i in range(len(textoSplit) - 1):
+                definicion += textoSplit[i]
+
+            if len(definicion) == 0:
                 definicion = "No hay definición en la wikipedia."
 
-            palabra = Palabra(nombrePalabra=pal, definicion=definicion, autor=autor, votos=votos)
+            palabra = Palabra(nombrePalabra=pal, definicion=definicion, autor=request.user, votos=0)
             palabra.save()
 
         elif action == 'Buscar palabra':
@@ -147,10 +155,7 @@ def get_palabra(request, pal):
             url += pal
             return redirect(url)
 
-        elif action == 'votar':
-            body = request.body.decode('utf-8')
-            partes_body = body.split('&')
-            pal = partes_body[1].split('=')[1]
+        elif action == 'votar palabra':
             for palabra in lista_palabras:
                 if pal == palabra.nombrePalabra:
                     print("traza palabra: " + pal)
@@ -169,7 +174,8 @@ def get_palabra(request, pal):
                     user.save()
                     login(request, user)
                 except IntegrityError:
-                    print("TRAZA - Error al crear usuario")  # -- TODO: Mensaje de error
+                    url = '/MisPalabras/error'
+                    redirect(url)
 
         elif action == 'Logout':
             logout(request)
@@ -192,24 +198,29 @@ def get_palabra(request, pal):
 
 
     añadida = palabraAñadida(pal)
+    try:
+        text = parsearWiki(pal)[0]
+        # Lo hacemos así para que no haya definiciones a medias
+        textoSplit = text.split('.')
+        definicion = ""
+        for i in range(len(textoSplit) - 1):
+            definicion += textoSplit[i]
 
-    text = parsearWiki(pal)[0]
-    # Lo hacemos así para que no haya definiciones a medias
-    textoSplit = text.split('.')
-    definicion = ""
-    for i in range(len(textoSplit) - 1):
-        definicion += textoSplit[i]
-
-    if len(definicion) == 0:
+        if len(definicion) == 0:
+            definicion = "No hay definición en la wikipedia."
+    except KeyError:
         definicion = "No hay definición en la wikipedia."
 
-    image = parsearWiki(pal)[1]  # -- TODO Meter imagen
+    image = parsearWiki(pal)[1]
+
+    tiene_imagen = image != "No hay"
 
     template = loader.get_template('MisPalabras/palabra.html')
 
     context = {'lista_palabras': lista_palabras, 'user': request.user.username, 'num_palabras': num_palabras,
                'palabraRandom': palabraRandom, 'palabra': pal, 'definicion': definicion, 'imagen': image,
-               'añadida': añadida, "votos": votos, 'lista_votadas': lista_mas_votadas , 'lista_comentarios': lista_coms}
+               'añadida': añadida, "votos": votos, 'lista_votadas': lista_mas_votadas , 'lista_comentarios': lista_coms,
+               'imagen': image, 'tiene_imagen': tiene_imagen}
 
     return HttpResponse(template.render(context, request))
 
@@ -242,7 +253,8 @@ def get_ayuda(request):
                     user.save()
                     login(request, user)
                 except IntegrityError:
-                    print("TRAZA - Error al crear usuario")  # -- TODO: Mensaje de error
+                    url = '/MisPalabras/error'
+                    redirect(url)
 
         if action == 'Logout':
             logout(request)
@@ -282,7 +294,9 @@ def get_usersPag(request):
                     user.save()
                     login(request, user)
                 except IntegrityError:
-                    print("TRAZA - Error al crear usuario")  # -- TODO: Mensaje de error
+                    url = '/MisPalabras/error'
+                    redirect(url)
+
 
         if action == 'Logout':
             logout(request)
@@ -295,11 +309,15 @@ def get_usersPag(request):
         if palabra.autor == request.user.get_username():
             palabras_añadidas.append(palabra)
 
+    palabras_añadidas = palabras_añadidas[::-1]
+
     comentarios_puestos = []
 
     for comentario in Comentario.objects.all():
         if comentario.autor == request.user.get_username():
             comentarios_puestos.append(comentario)
+
+    comentarios_puestos = comentarios_puestos[::-1]
 
 
     template = loader.get_template('MisPalabras/miPagina.html')
@@ -309,6 +327,42 @@ def get_usersPag(request):
 
     return HttpResponse(template.render(context, request))
 
+
+def get_error(request):
+    lista_palabras, num_palabras, palabraRandom = conseguirValoresPalabras()
+    template = loader.get_template('MisPalabras/error.html')
+    context = {'lista_palabras': lista_palabras, 'user': request.user.username, 'num_palabras': num_palabras,
+               'palabraRandom': palabraRandom}
+    return HttpResponse(template.render(context, request))
+
+
+def get_xml(request):
+    pass
+
+def get_json(request):
+    json_str = "{"
+    for palabra in Palabra.objects.all():
+        json_str += f"\n \t '{palabra.nombrePalabra}'" + "{ \n"
+        json_str += f"\t \t 'autor':{palabra.autor} \n"
+        json_str += f"\t \t 'definicion': {palabra.definicion} \n"
+        json_str += f"\t \t 'votos': {palabra.votos}" + "}\n"
+
+    json_str += "}\n?format=json"
+
+    file = open('MisPalabras.json', 'w')
+    try:
+        file.write(json_str)
+    finally:
+        file.close()
+
+    lista_palabras, num_palabras, palabraRandom = conseguirValoresPalabras()
+    lista_mas_votadas = crearListaMasVotadas()
+
+    template = loader.get_template('MisPalabras/json.html')
+    context = {'lista_palabras': lista_palabras, 'user': request.user.username, 'num_palabras': num_palabras,
+               'palabraRandom': palabraRandom, 'lista_votadas': lista_mas_votadas}
+
+    return HttpResponse(template.render(context, request))
 
 data = ['', '', '']
 
@@ -341,13 +395,20 @@ def parsearWiki(palabra):
     url_img = "https://es.wikipedia.org/w/api.php?action=query&titles=" + palabra + "&prop=pageimages&format=json&pithumbsize=300" # ????
 
     response = urllib.request.urlopen(url_img)
-    xmlStream = urllib.request.urlopen(url_text)
     jsonStream = json.loads(response.read())
+    xmlStream = urllib.request.urlopen(url_text)
     Parser = make_parser()
     Parser.setContentHandler(Handler())
     Parser.parse(xmlStream)
     texto = data[0]
 
-    # TODO Source de la imagen?
+    try:
+        image = jsonStream['query']["pages"][data[1]]["thumbnail"]["source"]
+    except KeyError:
+        print("No hay imagen")
+        image = "No hay"
+    finally:
+        print('imagen',image)
 
-    return texto[0:500], jsonStream['query']["pages"][data[1]]
+        return texto[0:500], image
+
